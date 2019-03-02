3b3a3828c540a82621e6ca97db7ab68690497c75
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-03-01 21:47:33 -0600

    CRUD

--- Clase7/src/app.js
+++ Clase7/src/app.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // app.use(bodyParser.json())
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}))
+ app.use(bodyParser.urlencoded({extended: true}))
 
 // Mongo URI
 const db = require('./config/keys').mongoURI;
